d2d4617feedafe4bfea5f5afd34589a95a86a31a
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2025-05-22 22:30:33 +0100

    new <ul> added for mobile navbar, responsive nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 
 
 function App() {
- 
 
   return (
     <div className="bg-slate-800 w-full overflow-hidden">
@@ -15,8 +14,17 @@ function App() {
           <li className="px-3"><a href="#client">Clients</a></li>
         </ul>
       </div>
-     </div>  
-    </div>
+     </div>
+
+        <ul className="flex flex-col sm:hidden justify-end items-start text-white">
+          <li className="px-3"><a href="#home">Home</a></li>
+          <li className="px-3"><a href="#features">Features</a></li>
+          <li className="px-3"><a href="#products">Product</a></li>
+          <li className="px-3"><a href="#pricing">Pricing</a></li>
+          <li className="px-3"><a href="#client">Clients</a></li>
+        </ul>
+      </div>
+    
   )
 }
 
