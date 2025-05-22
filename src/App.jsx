ea7039e424c3af9d8b14677e8ca8f7551faeaa83
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2025-05-22 22:11:11 +0100

    navbar created, navlinks added, styled, wide screen

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,18 @@ function App() {
  
 
   return (
-    <div className="w-[100%] h-[100vh] bg-lime-50">
-    <h1 className="text-center text-3xl text-blue-400">Hi Iva, I'm your banking app!</h1>  
+    <div className="bg-slate-800 w-full overflow-hidden">
+     <div className="flex sm:px-16 px-6 justify-center items-start">
+      <div className="xl:max-w-[1280px] w-full">
+        <ul className="sm:flex hidden h-14 justify-end items-center text-white">
+          <li className="px-3"><a href="#home">Home</a></li>
+          <li className="px-3"><a href="#features">Features</a></li>
+          <li className="px-3"><a href="#products">Product</a></li>
+          <li className="px-3"><a href="#pricing">Pricing</a></li>
+          <li className="px-3"><a href="#client">Clients</a></li>
+        </ul>
+      </div>
+     </div>  
     </div>
   )
 }
