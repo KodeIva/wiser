14ff9f812fd00d6b9ab2e5c2222c57e3a7304e2d
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2025-05-22 14:17:29 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+ 
+
+  return (
+    <div className="w-[100%] h-[100vh] bg-lime-50">
+    <h1 className="text-center text-3xl text-blue-400">Hi Iva, I'm your banking app!</h1>  
+    </div>
+  )
+}
+
+export default App
