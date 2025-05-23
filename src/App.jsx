1c10b31e855118e94a22834d1a40c72fe1d13b7a
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2025-05-23 20:16:08 +0100

    hero <div> created

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,16 @@ function App() {
    <div className="bg-slate-800 w-full overflow-hidden">
      <div className="flex sm:px-16 px-6 justify-center items-start">
       <div className="xl:max-w-[1280px] w-full">
-       {/* 
-
-        <ul className="flex flex-col sm:hidden justify-end items-start text-white">
-          <li className="px-3"><a href="#home">Home</a></li>
-          <li className="px-3"><a href="#features">Features</a></li>
-          <li className="px-3"><a href="#products">Product</a></li>
-          <li className="px-3"><a href="#pricing">Pricing</a></li>
-          <li className="px-3"><a href="#client">Clients</a></li>
-        </ul>*/}
          <Navbar />
       </div>
      </div> 
-     </div>  
+
+     <div className="bg-slate-800 flex justify-center items-start">
+       <div className="xl:max-w-[1280px] w-full">
+        Hero
+       </div>
+     </div>
+   </div>  
   
    
   
