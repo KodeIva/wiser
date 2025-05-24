7238ec3f23f71e1708f76bd2384f913663407a48
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2025-05-24 17:50:49 +0100

    <Hero/> added to 'App.js', <div> for discountand <div> with slogan added to <Hero/>

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 
 
@@ -13,7 +14,7 @@ function App() {
 
      <div className="bg-slate-800 flex justify-center items-start">
        <div className="xl:max-w-[1280px] w-full">
-        Hero
+        <Hero/>
        </div>
      </div>
    </div>  
---
+++ src/components/Hero.jsx
@@ -0,0 +1,30 @@
+import { FaPercentage } from "react-icons/fa";
+
+const Hero = () => {
+  return (
+    <section id="home" className="flex md:flex-row flex-row sm:py-16 py-6">
+      <div className="flex justify-center items-start flex-1 flex-col xl:px-0 sm:px-16 px-6">
+        
+        <div className="flex flex-row items-center py-[6px] px-4">
+          <FaPercentage className="text-blue-300 text-xl" />
+          <p className="text-[#9b9b9a] text-[18px] font-normal leading-[30px] ml-2">
+             <span className="text-white">20%</span> Discount For {" "}
+             <span className="text-white">1 Month</span> Account
+          </p>
+        </div>
+
+        <div className="flex flex-row justify-between items-center w-full">
+          <h1 className="flex-1 font-semibold md:text-[72px] text-[52px] text-white leadiing-[75px]">
+            The next <br />
+            <span className="bg-gradient-to-r from-blue-600  via-blue-400  to-blue-200 text-transparent bg-clip-text" >
+                Generation
+            </span> {" "} <br />
+            Payment Method </h1>
+        </div> 
+
+      </div>
+    </section>
+  )
+}
+
+export default Hero
