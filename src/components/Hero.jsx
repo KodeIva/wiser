32c646ac5be81926f14e4eee86e41c3bec638fab
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2025-05-25 09:44:19 +0100

    image renamed and added in new <div> inside <Hero/>, styled

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,10 @@
 import { FaPercentage } from "react-icons/fa";
 import GetStartedButton from "./GetStartedButton";
+import creditCard  from '../assets/creditCard.png'
 
 const Hero = () => {
   return (
-    <section id="home" className="flex md:flex-row flex-row sm:py-16 py-6">
+    <section id="home" className="flex lg:flex-row justify-center items-center flex-col sm:py-16 py-6">
       <div className="flex justify-center items-start flex-1 flex-col xl:px-0 sm:px-16 px-6">
         
         <div className="flex flex-row items-center py-[6px] px-4">
@@ -14,7 +15,7 @@ const Hero = () => {
           </p>
         </div>
 
-        <div className="flex flex-row justify-between items-center w-full">
+        <div className="flex md:flex-row flex-col justify-between items-center w-full">
           <h1 className="flex-1 font-semibold md:text-[72px] text-[52px] text-white leadiing-[75px] mb-5">
             The next <br />
             <span className="bg-gradient-to-r from-blue-600  via-blue-400  to-blue-200 text-transparent bg-clip-text" >
@@ -23,13 +24,17 @@ const Hero = () => {
             Payment Method 
           </h1>
 
-          <div>
+          <div className="flex"> 
             <GetStartedButton/>
           </div>
         </div> 
 
         <p className="max-w-[500px] text-white">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores, ipsam error vero magni tempora, impedit natus? Culpa provident delectus voluptates rem asper.</p>
 
+      </div>
+
+      <div className="flex-1 flex justify-center items-center w-[350px] md:w-[450px] md:h-[450px] ">
+        <img className="w-[100%] h-[100%] max-w-[350px] md:max-w-[450px] relative" src={creditCard} alt="" />
       </div>
     </section>
   )
