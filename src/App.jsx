ca5374018a36731b1cf18d6ea28dd9dcfc6bdced
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2025-05-25 10:59:22 +0100

    map() used to render through stats array, <Stats/> imported to App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
+import Stats from "./components/Stats"
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
      <div className="bg-slate-800 flex justify-center items-start">
        <div className="xl:max-w-[1280px] w-full">
         <Hero/>
+        <Stats />
        </div>
      </div>
    </div>  
--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { stats } from "../data/data"
 
 const Stats = () => {
   return (
     <section className='flex text-white justify-center items-center'>
-      
+      {stats.map((stat) => {
+        const { id, title, count } = stat
+        return (
+            <div key={id}>
+              <h4>{title}</h4>
+              <p>{count}</p>
+            </div>
+        )
+      })}
     </section>
   )
 }
