dbe5c810b82a6d3c271b1d7b3d4a27147eb5a285
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2025-06-18 22:20:28 +0100

    <div> & <img> added, styled

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -6,12 +6,16 @@ const CardDeal = () => {
       <div className='flex-1 flex flex-col justify-center items-center'>
        <h2 className='w-full font-semibold text-3xl text-white leading-10 '>Find the better card deal <br />
        in few easy steps.</h2>
-       <p className='text-[18px] leading-7 '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, maiores.</p>
+       <p className='text-[18px] text-[#9b9b9a]  leading-7 '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, maiores.</p>
       </div>
 
       <button type='button' className='mt-10 bg-gradient-to-r from-blue-600  via-blue-400  to-blue-200 py-4 px-6 font-medium text-lg text-slate-700 rounded-xl outline-none '>
         Get Started
       </button>
+
+      <div className="flex flex-1 justify-center items-center md:mr-10 mr-0 md:mt-0 mt-10 relative">
+         <img src={deal} className='w-[100%] h-[100%]' alt="" />
+      </div>
       
     </section>
   )
