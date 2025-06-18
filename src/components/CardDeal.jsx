e7cee32e0376eeb8bb6aaf6425ca74fcac7825a7
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2025-06-18 22:29:46 +0100

    items-center replaced with items-start

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -3,7 +3,7 @@ import deal from '../assets/deal.jpg'
 const CardDeal = () => {
   return (
     <section className='flex lg:flex-row flex-col sm:py-16 py-16'>
-      <div className='flex-1 flex flex-col justify-center items-center'>
+      <div className='flex-1 flex flex-col justify-center items-start'>
        <h2 className='w-full font-semibold text-3xl text-white leading-10 '>Find the better card deal <br />
        in few easy steps.</h2>
        <p className='text-[18px] text-[#9b9b9a]  leading-7 '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, maiores.</p>
